64dda00be9e26445414ec46eaaa86c35bb794479
LeckoAraujo <leandroedsonsa@gmail.com>
2022-02-09 14:07:47 -0300

    Limitando o espaço de sobe e desce das naves

--- assets/js/js.js
+++ assets/js/js.js
@@ -56,14 +56,22 @@ function start() { // Inicio da função start()
 
 		if (jogo.pressionou[TECLA.UP]) {
 			var topo = parseInt($("#jogador").css("top"));
-			$("#jogador").css("top",topo-10);
+			
+			if (topo>=434) {	
+				$("#jogador").css("top",topo-10);
+					
+			}
 		
 		}
 		
 		if (jogo.pressionou[TECLA.DOWN]) {
 			
 			var topo = parseInt($("#jogador").css("top"));
-			$("#jogador").css("top",topo+10);	
+			
+			if (topo<=0) {
+		
+				$("#jogador").css("top",topo+10);
+			}
 		}
 		
 		if (jogo.pressionou[TECLA.RIGHT]) {
