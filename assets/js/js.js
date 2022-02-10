2cb9780f0b635fb0891e04345e002e80bf698af2
LeckoAraujo <leandroedsonsa@gmail.com>
2022-02-10 10:01:40 -0300

    Adicionando função de disparo de tiros

--- assets/js/js.js
+++ assets/js/js.js
@@ -10,6 +10,7 @@ function start() { // Inicio da função start()
 
     //Principais variáveis do jogo
 	
+	var podeAtirar=true;
 	var jogo = {}
 	var velocidade=5;
 	var posicaoY = parseInt(Math.random() * 334);
@@ -81,7 +82,8 @@ function start() { // Inicio da função start()
 		
 		if (jogo.pressionou[TECLA.RIGHT]) {
 			
-			//Chama função Disparo	
+			//Chama função Disparo
+			disparo();
 		}
 	
 	} // fim da função movejogador()
@@ -126,6 +128,46 @@ function start() { // Inicio da função start()
 		}
 	
 	} // fim da função moveamigo()
+
+
+	function disparo() {
+	
+		if (podeAtirar==true) {
+			
+			podeAtirar=false;
+			
+			topo = parseInt($("#jogador").css("top"))
+			posicaoX= parseInt($("#jogador").css("left"))
+			tiroX = posicaoX + 180;
+			topoTiro=topo+52;
+			$("#fundoGame").append("<div id='disparo'></div");
+			$("#disparo").css("top",topoTiro);
+			$("#disparo").css("left",tiroX);
+			
+			var tempoDisparo=window.setInterval(executaDisparo, 30);
+		
+		} //Fecha podeAtirar
+	 
+		function executaDisparo() {
+			posicaoX = parseInt($("#disparo").css("left"));
+			$("#disparo").css("left",posicaoX+15); 
+
+				//Pode Atirar Livremente
+				/*tempoDisparo=null;
+				podeAtirar=true;*/
+	
+				//Só pode atirar depois do tira sair da tela
+				if (posicaoX>900) {
+							
+					window.clearInterval(tempoDisparo);
+					tempoDisparo=null;
+					$("#disparo").remove();
+					podeAtirar=true;
+						
+				}
+
+		} // Fecha executaDisparo()
+	} // Fecha disparo()
         
 
 } // Fim da função start
